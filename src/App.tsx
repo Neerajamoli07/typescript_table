3d282b2411c75890c146694073469faea2e69596
Neeraj Amoli <neeraj.amoli200@gmail.com>
2023-09-10 09:38:12 +0530

    test api responses integrated

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,16 @@ import "./App.css";
 
 //In typescript we have to defined every column data type what type that is
 interface Show {
+  enterprise_customer_id: string;
+  src_sys_cde: string;
+  src_sys_cust_nbr: string;
+  cust_kind_cde: string;
+  brth_dt: string;
+  sex_cde: string,
+  gndr_cde: string,
   frst_nm: string;
+  mddl_nm: string;
+  lst_nm: string;
   // type: string;
   // language: string;
   // officialSite: string;
@@ -23,8 +32,48 @@ function App() {
   </Explaintion> */
   const columns = [
     {
-      name: 'Name',
+      name: 'Enterprise Cust id',
+      selector: (row: any) => row.enterprise_customer_id,
+      sortable: true,
+    },
+    {
+      name: 'System code',
+      selector: (row: any) => row.src_sys_cde,
+      sortable: true,
+    },
+    {
+      name: 'Customer No.',
+      selector: (row: any) => row.src_sys_cust_nbr,
+      sortable: true,
+    },
+    {
+      name: 'Birthdate',
+      selector: (row: any) => row.brth_dt,
+      sortable: true,
+    },
+    {
+      name: 'Sex code',
+      selector: (row: any) => row.sex_cde,
+      sortable: true,
+    },
+    {
+      name: 'Gender',
+      selector: (row: any) => row.gndr_cde,
+      sortable: true,
+    },
+    {
+      name: 'First Name',
       selector: (row: any) => row.frst_nm,
+      sortable: true,
+    },
+    {
+      name: 'Middle Name',
+      selector: (row: any) => row.mddl_nm,
+      sortable: true,
+    },
+    {
+      name: 'Last Name',
+      selector: (row: any) => row.lst_nm,
       sortable: true,
     },
     // {
@@ -59,6 +108,10 @@ function App() {
   const [rowData, setRowData] = useState<any>('');
   const [isOpen, setIsOpen] = useState<any>(false);
   const [searchTerm, setSearchTerm] = useState("");
+  const [nativekey, setNativekey] = useState("");
+  const [enterprise, setEnterPrise] = useState("");
+  const [selectedDropdown, setSelectDropdown] = useState("");
+  const [dropdonData, setDropdownData] = useState<any[]>([]);
 
   // handle table cell click 
   const handleCellClick = (row: any, column: any) => {
@@ -112,8 +165,8 @@ function App() {
       }
       //const response = await axios.get<Show[]>('http://api.tvmaze.com/search/shows?q=animals');
 
-
-      setData([sample_response]);
+      console.log("Neeraj data", data);
+      setDropdownData([sample_response.src_sys_cde]);
 
     } catch (err: any) {
       const errorMessage = "Error: " + err.message;
@@ -130,38 +183,126 @@ function App() {
     setSearchTerm(e.target.value);
   }
 
+  // submit handler 
+  const submitHandler = () => {
+    if ((selectedDropdown !=="" && nativekey !== "") || (selectedDropdown !== "" && enterprise !== "")) {
+      try {
+        const sample_response = {
+          "enterprise_customer_id": "DEFAULT",
+          "src_sys_cde": "CIF  ",
+          "src_sys_cust_nbr": "301722990",
+          "cust_kind_cde": "INDIV",
+          "brth_dt": "2013-05-29",
+          "sex_cde": "M",
+          "gndr_cde": null,
+          "frst_nm": "Irving",
+          "mddl_nm": null,
+          "lst_nm": "Boat",
+          "orzn_nm": null,
+          "first_name_search": null,
+          "last_name_search": null,
+          "match_result": null,
+          "match_date_time": null,
+          "last_updated_date_time": null
+        }
+        //const response = await axios.get<Show[]>('http://api.tvmaze.com/search/shows?q=animals');
+  
+        setData([sample_response]);
+  
+      } catch (err: any) {
+        const errorMessage = "Error: " + err.message;
+        console.log(errorMessage);
+      } finally {
+        console.log("finally");
+      }
+    }else {
+      alert("Please select appropriat option...");
+    }
+
+  }
+
+  // cancel handler 
+  const cancelHandler = () => {
+     setNativekey("");
+     setEnterPrise(""); 
+     setSelectDropdown("");
+     setData([]);
+  }
+
+
   return (
     <div className="App">
-        <div className="data-table-search">
-          <label className="icon"></label>
-          <input type="text" 
-                 value={searchTerm}
-                 name="filterDataTable" 
-                 onChange={searchHandler} 
-                 className="filter-text" 
-                 placeholder="Search"/>
+        <div className="section">
+          <div className="section__title">
+            <h6>Search By Customer</h6>
+            <p>*One of the following is required: Enterprise ID, Native key/source system</p>
+         </div>
+         <div className="section__body">
+            <div className="source_system">
+              <select id="source_dropdown" 
+                      value={selectedDropdown} 
+                      onChange={(e) => setSelectDropdown(e.target.value)}>
+                <option value="">Select</option>
+                {dropdonData && (
+                  dropdonData?.map((item :any , index)=>{
+                   return <option key={index} value={item.trim()}>{item.trim()}</option>
+                  })
+                )}
+              </select>
+              <input type="text" 
+                 value={nativekey}
+                 name="native_key"
+                 onChange={(e) => setNativekey(e.target.value)} 
+                 className="native__key" 
+                 placeholder="Native Key"/>
+            </div>
+            <div className="enterprise__search">
+              <input type="text" 
+                 value={enterprise}
+                 onChange={(e) => setEnterPrise(e.target.value)}
+                 name="enterprise" 
+                 className="enterprise" 
+                 placeholder="Enterprise Id"/>
+            </div>
+            <div className="section__buttons">
+               <button className="action__buton" onClick={submitHandler}>Submit</button>
+               <button className="action__buton" onClick={cancelHandler}>Reset</button> 
+            </div>
+         </div>
+
         </div>
-        <DataTable
-          columns={columns}
-          data={data}
-          highlightOnHover
-          pagination
-          paginationPerPage={5}
-          paginationRowsPerPageOptions={[5, 10, 15, 25, 50]}
-          paginationComponentOptions={{
-            rowsPerPageText: 'Records per page:',
-            rangeSeparatorText: 'out of',
-          }}
-        />
-      <Popup open={isOpen} modal nested onClose={() => setIsOpen(false)}>
-        {
-          (
-            <> 
-                { rowData } 
-            </>
-          )
-        }
-      </Popup>
+        { data.length > 0 ? (<div className="section__table_body"> 
+          <div className="data-table-search">
+            <label className="icon"></label>
+            <input type="text" 
+                  value={searchTerm}
+                  name="filterDataTable" 
+                  onChange={searchHandler} 
+                  className="filter-text" 
+                  placeholder="Search"/>
+          </div>
+          <DataTable
+            columns={columns}
+            data={data}
+            highlightOnHover
+            pagination
+            paginationPerPage={5}
+            paginationRowsPerPageOptions={[5, 10, 15, 25, 50]}
+            paginationComponentOptions={{
+              rowsPerPageText: 'Records per page:',
+              rangeSeparatorText: 'out of',
+            }}
+          />
+        <Popup open={isOpen} modal nested onClose={() => setIsOpen(false)}>
+          {
+            (
+              <> 
+                  { rowData } 
+              </>
+            )
+          }
+        </Popup>
+      </div>) : null}
     </div>
   );
 }
